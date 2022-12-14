b8668ad97d384e74dd7195e58920415d6ca29d41
muhdrayan <muhammedrayansavad1@gmail.com>
2022-12-14 23:23:14 +0300

    Game Play Update Part 3

--- Backend/gameplay.py
+++ Backend/gameplay.py
@@ -9,6 +9,39 @@ team2 = {'GK': [('Alexis Frederic', 89, 'GK')], 'LB': [('Berry Ronald', 91, 'LB'
  'RB': [('Alton Deon', 80, 'RB')], 'LM': [('Monte Charlie', 80, 'LM')], 'CM': [('Eugenio Dillon', 71, 'CM')], 'RM': [('Stacey Douglass', 76, 'RM')],
  'LW': [('Alfredo Tommy', 83, 'LW')], 'ST': [('Boris Thomas', 84, 'ST')], 'RW': [('Ward Trevor', 89, 'RW')]}
 
+class Player:
+    def __init__(self, name, rating, position, team):
+        self.name = name
+        self.rating = rating
+        self.position = position
+        self.team = team
+
+        #stats
+        self.goals = 0
+        self.assists = 0
+        
+        #cards
+        self.yellow = 0
+        self.red = 0
+
+class Team:
+    def __init__(self, name, starting11, playing_style):
+        self.name = name
+        self.s11 = starting11
+
+        self.style = playing_style
+
+        self.assign_players()
+
+    def assign_players(self):
+        for pos, players in self.s11.items():
+            p = []
+            for player in players:
+                p.append(Player(player[0], player[1], player[2], self.name))
+            
+            exec(f"self.{pos} = {p}")
+
+
 class Chance:
     def __init__(self, team1, team2):
         self.position = random.choice(["defense", "midfield", "attack"])    
@@ -21,23 +54,49 @@ class Chance:
         with open("./Data/gameplay.json", "r") as f:
             self.gameplay_data = json.load(f)
 
-        self.offense() if self.team1_chance else self.defense()
+        self.chance("offense") if self.team1_chance else self.chance("defense") 
 
-    def offense(self):
-        self.gameplay_data = self.gameplay_data["offense"][self.position]
+    def chance(self, mode):
+        self.gameplay_data = self.gameplay_data[mode][self.position]
         self.chance_type = random.choice(list(self.gameplay_data.keys()))
 
         self.options = list(self.gameplay_data[self.chance_type]["options"].keys())
         print(self.chance_type, self.position, self.options)
 
-    def defense(self):
-        self.gameplay_data = self.gameplay_data["defense"][self.position]
-        self.chance_type = random.choice(list(self.gameplay_data.keys()))
 
-        self.options = list(self.gameplay_data[self.chance_type]["options"].keys())
-        print(self.chance_type, self.position, self.options)
+class Match:
+    def __init__(self, team1, team2):
+        self.team1 = team1
+        self.team2 = team2
 
-Chance(team1, team2)
+        self.time = 0
+        self.extra_time = random.randint(0, 8)
+        self.score = (0, 0)
+
+    def start_match(self):
+
+        chance_weights = {'oo': (50, 50), 'om': (60, 40), 'od': (75, 25),
+                        'mm': (50, 50), 'md': (60, 40), 'dd': (50, 50)}
+        
+        clash = f"{self.team1.style}{self.team2.style}"
+        weight = chance_weights[clash if clash in chance_weights else f"{self.team2.style}{self.team1.style}"]
+
+        while self.time > 90 + self.extra_time:
+            chance = Chance(self.team1, self.team2)
+            self.execute_chance(chance)
+            
+            self.time += random.randint(0, 10)
+            
+    def execute_chance(self, chance):
+        pass
+            
+
+
+
+
+            
+
+
 
 
 
